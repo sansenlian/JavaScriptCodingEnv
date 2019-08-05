011e95f4ab964bd688f579478e6700c0c749f4b1
AchunOrigin <sansenlian@foxmail.com>
2019-08-05 16:43:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# js-coding-env
---
+++ index.js
@@ -0,0 +1,43 @@
+import {read as readfile, print,_remaind} from './dist/file-io';
+
+var _line_index = 0;
+readfile().then(_data=>{
+    _remaind.warn(_data)
+    _data = _data.split('\n')
+    function readline() {
+        let res = _data[_line_index];
+        _remaind.warn("读取一行字符串: "+res)
+        _line_index++;
+        return res;
+    }
+    _remaind.success("------------------------------- 以下是自己的结果")
+    customCode(readline,readline);
+});
+/**
+ * @method  customCode      笔试的时候代码写在这个函数里
+ * @param   {readline}      牛客网读取一行
+ * @param   {read_line}     赛码网读取一行
+ * @author                  sansenlian@foxmail.com
+ * @date                    2019年08月05日11:00:38
+ * @warning                 读取的是字符串, 一般如果是数字的话就要全部转成数字
+ */
+function customCode(readline,read_line) {
+    // ========================================== 以下是自己的代码, 复制到编辑器
+    let line;
+    function _newline(_line_in) {
+        let _newline_arr = [];
+        _line_in = _line_in.split(" ");
+        let _line_length = _line_in.length;
+        for(let i = 0;i<_line_length;i++)
+            _newline_arr[i] = Number(_line_in[i]);
+        return _newline_arr;
+    }
+    // ------------------------------------------------------- 编辑笔试部分代码
+
+
+
+
+
+
+    // ========================================== 编辑结束
+}
